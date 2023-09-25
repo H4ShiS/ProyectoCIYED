6f5940b8290aafcdb3a471cf42637b3ddc3e604c
Jonny <santojonny12@gmail.com>
2023-09-25 00:48:52 -0600

    primeras creaciones de los textfield al igual que el estilo que se esta formando

--- lib/src/pages/login/login_seccion.dart
+++ lib/src/pages/login/login_seccion.dart
@@ -37,6 +37,62 @@ class loginUser extends StatelessWidget {
                 //agregar la imagen 
                 backgroundImage: const AssetImage('assets/images/LogoGowPlay.png'),
               ),
+
+              const SizedBox(height: 20,),
+
+
+              // const Text(
+              //   "Login",
+              //   style: TextStyle(),
+              // ),
+
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  hintText: "Correo Electronico"
+                ),   
+              ),
+
+
+              const SizedBox(height: 20,),
+
+
+              TextField(
+                decoration: InputDecoration(
+                  
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0)
+                  ),
+                  hintText: "Contraseña"
+                ),   
+              ),
+
+              const SizedBox(height: 20,),
+
+              InkWell(
+                borderRadius: BorderRadius.circular(15.0), // Establecer el radio de borde
+                onTap: () {
+                  // Manejar el evento de pulsación
+                },
+                child: Material(
+                  borderRadius: BorderRadius.circular(15.0), // También puedes aplicar el radio de borde al Material
+                  color: Colors.lightBlue,
+                  child: const SizedBox(
+                    width: 200.0,
+                    height: 50.0,
+                    child: Center(
+                      child: Text(
+                        'Inicio Sesion',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+
+
             ],
           )
         ],
