6657e6806f8dee12c3b4fda8c142575c8e7494c2
Jonny <santojonny12@gmail.com>
2023-10-05 01:50:54 -0600

    login terminado con animaciones incluidas

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -1,52 +1,71 @@
+import 'package:app_ciyed/presentation/widgets/Animatios/Animaciones.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:simple_animations/simple_animations.dart';
 
-class LoginScreen extends StatelessWidget {
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<LoginScreen> createState() => _LoginScreenState();
+}
 
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
     // final colors = Theme.of(context).colorScheme;
+    final formField = GlobalKey<FormState>();
+    final emailController = TextEditingController();
+    final passController = TextEditingController();
+    bool passToggle = true;
+
     return Scaffold(
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Color.fromARGB(255, 6, 30, 65),
-              Color.fromARGB(255, 10, 62, 141),
-              Color.fromARGB(255, 2, 70, 172),
-            ]
-          )
-        ),
-        child: ListView( // Cambiado a ListView
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          // Color.fromARGB(255, 6, 30, 65),
+          // Color.fromARGB(255, 10, 62, 141),
+          // Color.fromARGB(255, 2, 70, 172),
+
+          Color.fromARGB(255, 177, 50, 11),
+          Color.fromARGB(255, 215, 75, 33),
+          Color.fromARGB(255, 228, 78, 32),
+        ])),
+        child: ListView(
+          // Cambiado a ListView
           children: <Widget>[
+            const SizedBox(
+              height: 5,
+            ),
 
-            const SizedBox(height: 5,),
-
-             const Stack(
+            const Stack(
               children: [
                 Padding(
                   padding: EdgeInsets.all(10),
 
                   child: Column(
-                   children: [
-                     Center(
-                       child: Positioned(
-                        top: 30, // Ajusta la posición vertical según tus necesidades
-                        left:20, // Ajusta la posición horizontal según tus necesidades
-                        child: CircleAvatar(
-                          radius: 150, // Tamaño del avatar
-                          backgroundColor: Color.fromARGB(255, 25, 19, 19), // Color de fondo
-                          backgroundImage:  AssetImage('assets/images/logociyed.png'),
+                    children: [
+                      Center(
+                        child: Positioned(
+                          top:
+                              30, // Ajusta la posición vertical según tus necesidades
+                          left:
+                              20, // Ajusta la posición horizontal según tus necesidades
+                          child: CircleAvatar(
+                            radius: 130, // Tamaño del avatar
+                            backgroundColor: Color.fromARGB(
+                                255, 255, 255, 255), // Color de fondo
+                            backgroundImage:
+                                AssetImage('assets/images/logociyed.png'),
+                          ),
                         ),
-                                     ),
-                     ),
-                   ],
-                 ),
-                  
-                  // child: 
+                      ),
+                    ],
+                  ),
+
+                  // child:
                   // Column(
                   //   crossAxisAlignment: CrossAxisAlignment.start,
                   //   children: <Widget>[
@@ -64,96 +83,158 @@ class LoginScreen extends StatelessWidget {
                   //   ],
                   // ),
                 ),
-
-                
-                
-                 
               ],
             ),
 
             // FloatingActionButton(onPressed: (){},
             //     backgroundColor: Colors.blue, child: const Icon(Icons.add),),
 
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
 
             Expanded(
               child: Container(
                 decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(60), topLeft: Radius.circular(60))
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(40),
+                        topLeft: Radius.circular(40)),
+
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white,
+                        blurRadius: 20,
+                        offset: Offset(5, 10),
+                      )
+                    ]),
+
                 child: Padding(
                   padding: const EdgeInsets.all(20),
                   child: Column(
                     children: <Widget>[
+                      const SizedBox(
+                        height: 50,
+                      ),
 
-                      const SizedBox(height: 60,),
                       Container(
                         decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Color.fromRGBO(255, 95, 27, 0.3),
-                              blurRadius: 20,
-                              offset: Offset(0, 10),
-                            )
-                          ]
-                        ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 243, 231, 231),
+                                blurRadius: 15,
+                                offset: Offset(1, 1),
+                              )
+                            ]),
                         child: Column(
                           children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey.shade200),
-                                )
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                  hintText: "Correo Electronico",
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              controller: emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                
+                                label: FadeAnimation(1, const Text("Correo Electronico")),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                prefixIcon: const Icon(
+                                  Icons.email,
+                                  color: Color.fromARGB(255, 6, 30, 65),
                                 ),
+                                labelStyle: const TextStyle(
+                                  fontSize: 15,
+                                  color: Color.fromARGB(255, 6, 30, 65),
+                                ),
+                                // filled: true,
+                                // fillColor:Color.fromARGB(255, 6, 30, 65),
                               ),
                             ),
-
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey.shade200),
-                                )
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                  hintText: "Password",
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none
-                                ),
-                              ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            TextFormField(
+                              controller: passController,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: InputDecoration(
+                                  label: FadeAnimation(1, const Text("Password")),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  prefixIcon: const Icon(
+                                    Icons.lock,
+                                    color: Color.fromARGB(255, 4, 45, 106),
+                                  ),
+                                  labelStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 4, 45, 106),
+                                    fontSize: 15,
+                                  ),
+                                  suffix: InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        passToggle = !passToggle;
+                                      });
+                                    },
+                                    // ignore: dead_code
+                                    child: Icon(passToggle
+                                        ? Icons.visibility
+                                        // ignore: dead_code
+                                        : Icons.visibility_off),
+                                  )),
                             )
-
                           ],
                         ),
-
                       ),
 
-                      const SizedBox(height: 50,),
-
-                      Container(
+                      const SizedBox(
                         height: 50,
-                        margin: const EdgeInsets.symmetric(horizontal: 50),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Color.fromARGB(255, 202, 73, 4)
-                        ),
-                        child: const Center(
-                          child: Text("Iniciar Sesion", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),),
+                      ),
+
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 4, 45, 106),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 35, 10, 143),
+                                  blurRadius: 10,
+                                  offset: Offset(2, 2),
+                                )
+                              ]),
+                          child:  Center(
+                            child: FadeAnimation(
+                              1, const Text(
+                                "Log out",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
 
-                      const SizedBox(height: 180,)
+                      // Container(
+                      //   height: 50,
+                      //   margin: const EdgeInsets.symmetric(horizontal: 50),
+                      //   decoration: BoxDecoration(
+                      //     borderRadius: BorderRadius.circular(50),
+                      //     color: const Color.fromARGB(255, 202, 73, 4)
+                      //   ),
+                      //   child: const Center(
+                      //     child: Text("Iniciar Sesion", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),),
+                      //   ),
+                      // ),
+
+                      const SizedBox(
+                        height: 150,
+                      )
                     ],
                   ),
                 ),
@@ -165,8 +246,6 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
-
-
 
 /*
         child: Column(
@@ -199,4 +278,4 @@ class LoginScreen extends StatelessWidget {
 
           ],
         ),
-*/ 
+*/
